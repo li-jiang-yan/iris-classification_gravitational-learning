98306eecb50fea03c923e3e2bb21cc9128eae119
li-jiang-yan <professional.jiangyan_li@outlook.com>
2025-08-27 13:50:26 +0300

    Added the scaling of data points

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
 
+# Load dataset
 iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+# Scale data
+X = StandardScaler().fit(X).transform(X)
